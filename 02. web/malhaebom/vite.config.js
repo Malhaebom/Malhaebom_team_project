867f9491ce16c7ab8bb56772b1365cdf3bfe0a6f
JKU <smfqh13@gmail.com>
2025-09-04 18:05:54 +0900

    수정

--- 02. web/malhaebom/vite.config.js
+++ 02. web/malhaebom/vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+    server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
